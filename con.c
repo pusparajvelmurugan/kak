dd1200731c42c42fa7322c767e2ba08db86531ba
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-02-09 16:04:03 +0530

    Create con.c

---
+++ con.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[20];
+char b[20];
+printf("enter the first string");
+scanf("%s",a);
+printf("enter the second string");
+scanf("%s",b);
+printf("%s%s",a,c);
+getch();
+}
